e8374e7f5086cdf1fb6f642545a87cd344155641
twqDaniell <thewickedqueend@gmail.com>
2025-01-05 23:01:44 +0200

    move files and create tests for what i already  have

---
+++ src/tests/posts.test.ts
@@ -0,0 +1,74 @@
+import request from "supertest";
+import appInit from "../server";
+import mongoose from "mongoose";
+import postsModel from "../models/posts_model";
+
+import testPostsData from "./test_posts.json";
+import { Express } from "express";
+
+let app: Express;
+
+type Post = {
+  _id?: string;
+  title: string;
+  content: string;
+  sender: string;
+};
+
+let testSender: String = "Daniel1";
+
+const testPosts: Post[] = testPostsData;
+
+beforeAll(async () => {
+  console.log("Before all tests");
+  app = await appInit();
+  await postsModel.deleteMany();
+});
+
+afterAll(() => {
+  console.log("After all tests");
+  mongoose.connection.close();
+});
+
+describe("Posts Test", () => {
+  test("Test get all post empty", async () => {
+    const response = await request(app).get("/posts");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(0);
+  });
+
+  test("Test create new post", async () => {
+    for (let post of testPosts) {
+      const response = await request(app).post("/posts").send(post);
+      expect(response.statusCode).toBe(201);
+      expect(response.body.title).toBe(post.title);
+      expect(response.body.content).toBe(post.content);
+      post._id = response.body._id;
+    }
+  });
+
+  test("Test get all post", async () => {
+    const response = await request(app).get("/posts");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(testPosts.length);
+  });
+
+  test("Test get post by id", async () => {
+    const response = await request(app).get("/posts/" + testPosts[0]._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body._id).toBe(testPosts[0]._id);
+  });
+
+  test("Test filter post by owner", async () => {
+    const response = await request(app).get("/posts?sender=" + testSender);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(2);
+  });
+
+  test("Test create new post fail", async () => {
+    const response = await request(app).post("/posts").send({
+      content: "Test Content 1",
+    });
+    expect(response.statusCode).toBe(400);
+  });
+});
