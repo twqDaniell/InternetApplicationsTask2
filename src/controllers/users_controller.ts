76c32d0c5679a8d1ec7c364c5c3509ed568bd94e
twqDaniell <thewickedqueend@gmail.com>
2025-01-06 15:32:06 +0200

    update and delete user

--- src/controllers/users_controller.ts
+++ src/controllers/users_controller.ts
@@ -3,7 +3,7 @@ import { Model } from "mongoose";
 import userModel, { IUser } from "../models/users_model";
 
 class UsersController<IUser> {
-    user: Model<IUser>;
+  user: Model<IUser>;
   constructor(user: Model<IUser>) {
     this.user = user;
   }
@@ -17,7 +17,34 @@ class UsersController<IUser> {
     } catch (error) {
       res.status(400).send(error);
     }
-  };
-};
+  }
+
+  async updateUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    const updateData = req.body;
+
+    try {
+      const updatedUser = await this.user.findByIdAndUpdate(
+        userId,
+        updateData,
+        { new: true }
+      );
+      res.status(200).send(updatedUser);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+  async deleteUser(req: Request, res: Response) {
+    const userId = req.params.id;
+
+    try {
+      await this.user.findByIdAndDelete(userId);
+      res.status(200).send();
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+}
 
 export default new UsersController(userModel);
--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -3,5 +3,7 @@ const router = express.Router();
 import usersController from "../controllers/users_controller";
 
 router.post("/", usersController.createUser.bind(usersController));
+router.put("/:id", usersController.updateUser.bind(usersController));
+router.delete("/:id", usersController.deleteUser.bind(usersController));
 
 export default router;
