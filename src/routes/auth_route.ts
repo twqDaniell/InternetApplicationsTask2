6fcbc12904355ad6095493ed72c2f44ab674b426
twqDaniell <thewickedqueend@gmail.com>
2025-01-07 15:18:06 +0200

    create the routes

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -2,6 +2,8 @@ import express from "express";
 const router = express.Router();
 import authController from "../controllers/auth_controller";
 
-router.post("/", authController.register.bind(authController));
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 export default router;
