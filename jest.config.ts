6ebf137bbd9fd80fe5842826b1b679834b29e42e
twqDaniell <thewickedqueend@gmail.com>
2025-01-12 21:40:07 +0200

    add update tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["html", "text", "lcov"],
 };
